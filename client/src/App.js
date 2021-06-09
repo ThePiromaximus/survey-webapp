2779499baa504606c0dde67b5cfb9c421cb74e3d
Gabriele Inzerillo <gabry982010@hotmail.com>
2021-06-09 12:03:44 +0200

    feat: added SurveysList and SurveyPreview

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './NavigationBar';
+import SurveysList from './LeftBar.js';
+import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+      <Container fluid className="min-vh-100">
+        <Row>
+          <Col md={3} className="p-3 bg-light" id="LeftBar">
+            <SurveysList></SurveysList>
+          </Col>
+          <Col md={9} className="min-vh-100">
+            Survey Form here
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
